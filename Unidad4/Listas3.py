70c00c55c93e1835c9a6494991cc48be0b759ce4
Sebastian5050942 <122662766+Sebastian5050942@users.noreply.github.com>
2026-09-15 11:33:58 -0500

    Add files via upload

---
+++ Unidad4/Listas3.py
@@ -0,0 +1,17 @@
+# Simulando lecturas de altitud cada 10 segundos
+altitudes = [0, 100, 500, 1000, 1500, 2000, 2200, 2500]
+tiempo = 0
+i = 0
+
+while i < len(altitudes):
+    print(f"Tiempo: {tiempo}s - Altitud: {altitudes[i]} metros")
+    tiempo += 10
+    i += 1
+
+for i in range(len(altitudes)): #range genera los indices
+    print(f"Tiempo: {tiempo}s - Altitud: {altitudes[i]} metros")
+    tiempo += 10
+
+for altitud in altitudes: #accedo directamente a los elementos
+      print(f"Tiempo: {tiempo}s - Altitud: {altitudes[i]} metros")
+      tiempo += 10
