839b43d8e110fd08e0a571e39bca5fd59bedfbae
Sebastian5050942 <122662766+Sebastian5050942@users.noreply.github.com>
2026-09-15 10:46:54 -0500

    Add files via upload

---
+++ Unidad4/actividad1.py
@@ -0,0 +1,16 @@
+# Creamos un objeto (en este caso, un número)
+altitud = 10000  # metros
+
+# 'altitud' es una etiqueta que apunta al objeto entero 10000
+# Podemos crear otra etiqueta que apunte al mismo objeto
+elevacion = altitud
+
+# Si modificamos el valor al que apunta 'elevacion'
+elevacion = 9500
+
+# 'altitud' sigue apuntando al valor original
+print(altitud)  # 10000
+print(elevacion)  # 9500
+
+altitud = "muy alto"
+print(altitud)
