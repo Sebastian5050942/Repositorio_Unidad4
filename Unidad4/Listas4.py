0cd299564cda737aad908a43669bad5e50a1991c
Sebastian5050942 <122662766+Sebastian5050942@users.noreply.github.com>
2026-09-17 10:25:25 -0500

    Add files via upload

---
+++ Unidad4/Listas4.py
@@ -0,0 +1,22 @@
+# Lista vacía
+componentes = []
+
+# Lista con elementos
+componentes = ["alas", "fuselaje", "motores", "tren de aterrizaje"]
+
+# Lista con diferentes tipos de datos
+datos_vuelo = [202, "Boeing 737", True, 10500.5]
+
+# Listas anidadas
+matriz_rotacion = [[56, 33, 57], [78, 13, 21], [17, 99, 1]]
+
+print(componentes[2])
+print(matriz_rotacion[1][2])
+print(matriz_rotacion[2])
+
+filas = len(matriz_rotacion)
+col = len(matriz_rotacion[0])
+
+comp2 = componentes[2:]
+print(comp2)
+
